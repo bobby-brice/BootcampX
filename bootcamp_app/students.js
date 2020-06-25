6e2c0bd6fb95746579cf2089d137e9930a472317
bobby-brice <bobby.brice@gmail.com>
2020-06-25 00:47:00 +0000

    parameterize queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,13 +9,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+const query = `
 SELECT students.id, students.name, cohorts.name as cohort_name
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2
+`;
+
+pool.query(query, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
